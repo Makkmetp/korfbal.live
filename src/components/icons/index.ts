603a76fdd55c64c07708e1791d4ba8f278d37089
Rogier Mechielsen <mail@rogiermechielsen.nl>
2026-03-24 16:52:31 +0100

    Add new images and components for enhanced user experience
    - Introduced new images for the live dashboard, live stream, and statistics to improve visual content.
    - Added Eyebrow and FeatureList components for better content structuring on the features page.
    - Implemented new icons for close and menu functionalities to enhance navigation.
    - Updated Navbar component to include a toggle for mobile view, improving accessibility and usability.
    - Created new pages for subscription and general terms, expanding site content and user information.

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as IconBrandFacebook } from "./IconBrandFacebook.astro";
 export { default as IconBrandInstagram } from "./IconBrandInstagram.astro";
 export { default as IconBrandLinkedin } from "./IconBrandLinkedin.astro";
+export { default as IconClose } from "./IconClose.astro";
+export { default as IconMenu } from "./IconMenu.astro";
