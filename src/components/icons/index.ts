972e55198c427678150851aa6a7e50dc45ca9af5
Rogier Mechielsen <mail@rogiermechielsen.nl>
2026-03-24 11:59:33 +0100

    Enhance footer component and add icon generation script
    - Updated Footer component to include social media icons for Facebook, Instagram, and LinkedIn.
    - Introduced a new script for generating Astro components from SVG icons, improving icon management.
    - Added new SVG icons for Facebook, Instagram, and LinkedIn to the assets.
    - Refactored package.json to include concurrently for running multiple scripts simultaneously.

---
+++ src/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as IconBrandFacebook } from "./IconBrandFacebook.astro";
+export { default as IconBrandInstagram } from "./IconBrandInstagram.astro";
+export { default as IconBrandLinkedin } from "./IconBrandLinkedin.astro";
